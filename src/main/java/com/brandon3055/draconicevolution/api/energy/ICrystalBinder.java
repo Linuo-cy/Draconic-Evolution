c8a8927f408609669dd8b3efc4c913f3014b8e92
Linuo-cy <3369267731@qq.com>
2026-05-25 02:13:56 +0800

    Delete src directory

--- src/main/java/com/brandon3055/draconicevolution/api/energy/ICrystalBinder.java
+++
@@ -1,8 +0,0 @@
-package com.brandon3055.draconicevolution.api.energy;
-
-/**
- * Created by brandon3055 on 25/11/2016.
- * Simply implement this interface on an item and it will be usable as a binder. This should not effect any of your tools other functionality.
- */
-public interface ICrystalBinder {
-}
